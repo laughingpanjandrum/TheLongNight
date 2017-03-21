041964d5e74aadf3b321d28ca6e6485a821de677
coryi <cory.isaak@gmail.com>
2017-03-20 20:13:11 -0700

    Added framework for a world map, though it remains unimplemented.

--- TheLongNight/worldmap.h
+++ TheLongNight/worldmap.h
@@ -2,14 +2,40 @@
 #ifndef WORLDMAP_H
 #define WORLDMAP_H
 
+
+#include <string>
+#include <vector>
+#include <memory>
+
 #include "libtcod.hpp"
+#include "map.h"
+
+
+struct mapMemoryPiece
+{
+	mapMemoryPiece(std::string name, TCODColor color) :
+		mapName(name), mapColor(color) {}
+	std::string mapName;
+	TCODColor mapColor;
+	bool isUnknown = true;
+	mapMemoryVector1d mapConnections;
+};
+
+typedef std::shared_ptr<mapMemoryPiece> mapMemorySharedPtr;
+typedef std::vector<mapMemorySharedPtr> mapMemoryVector1d;
+
+
 
 class worldmap
 {
 public:
+
 	worldmap();
 	~worldmap();
+
 protected:
+
+	mapMemoryPiece startPt;
 
 };
 
